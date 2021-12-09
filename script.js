0786c665d4035976347ee5dd648be665c0352991
Ken Dang <k3ndang@gmail.com>
2021-12-09 13:06:55 -0600

    fingure how to find chilren and change it without using class

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ $(document).ready(onReady);
 
 function onReady(){
  	console.log('on ready');
-    $('.title').text('Ken is learning Jquery');  // changing the h1 text in header using class
+    //$('.title').text('Ken is learning Jquery');  // changing the h1 text in header using class
    
-    $('#top').first('h1').css({
+    $('#top').css({
         color: 'green',
         'font-size': '28px'
     })
 
-
+    $('header').first().find('h1').text('Ken');  // select header, find first element of h1 and change text
 
 
 
